e5fc8f8dd8e2c6d56c45bdf5c124dba152aeeb7d
Evandr0 <evandro_lr_@hotmail.com>
2020-07-30 23:56:47 -0300

    Codigo copiado do repositorio da aula

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-/* eslint-disable linebreak-style */
-// eslint-disable-next-line import/no-extraneous-dependencies
-const jsonServer = require('json-server');
-
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
 const port = process.env.PORT || 8080;
 
-server.use(middlewares);
-server.use(router);
+server.use(middlewares)
+server.use(router)
 server.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`JSON Server is running in ${port}`);
-});
+  console.log(`JSON Server is running in ${port}`)
+}) 
